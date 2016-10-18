81767e009b705abadb577297cd5b0c75e80c692d
George Alegre <facu.alegre@gmail.com>
2016-10-18 13:35:31 -0300

    #26 Login form ready. Handle data submission.

--- src/goal_set_track/gst/views.py
+++ src/goal_set_track/gst/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse as HTTPr
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
+def home(request):
+    return HTTPr('Hola!')
+
+'''
+def login(request):
+    # print request.method
+    # TODO Render login template.
+    # viewitems = {}
+    # viewitems.title = 'GST Login'
+    # return render(request, 'gst/login.html', viewitems)
+    return HTTPr('Estas en el login.')
+'''
+
+class LoginView(View):
+    @csrf_protect
+    def get(self, request):
+        viewitems = {
+        }
+        return render(request, 'login.html', viewitems)
+
+    def post(self, request):
+        print request.POST
+
