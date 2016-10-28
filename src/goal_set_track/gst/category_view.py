0fa179c1df20e10cce7049ff351a16c494554213
zaloogarcia <gonzaloblue@gmail.com>
2016-10-27 21:02:58 -0300

    #122 view was splitted

---
+++ src/goal_set_track/gst/category_view.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.http import HttpResponse as HTTPr
+from .models import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class CategoryView(LoginRequiredMixin, View):
+    def get(self, req):
+        viewitems = {
+            'title': 'Categories',
+            'username': req.user.username,
+            'categories': req.user.category_set.all()
+        }
+        return render(req, 'gst/category.html', viewitems)
+
+class CategoryCreateView(LoginRequiredMixin, View):
+    def post(self, req):
+        name = req.POST['name']
+        req.user.category_set.create(name = name)
+        return redirect('task', category=req.user.category_set.get(name=name).id)
+
+class CategoryEditView(LoginRequiredMixin, View):
+    def get(self, req):
+        viewitems = {
+        }
+        categorys = Category.objects.filter(user = u)
+        return render(req, 'gst/category_delete.html', {'categorys': categorys, 'edit':True})
+
+    def post(self, req):
+        oldName = req.POST['oldName']
+        newName = req.POST['newName']
+        Category.objects.filter(name=oldName).filter(user=u).update(name=newName)
+        return HTTPr('Successful edited the category')
+
+class CategoryDeleteView(LoginRequiredMixin, View):
+    def get(self, req):
+        viewitems = {
+        }
+        categorys = Category.objects.filter(user = u)
+        return render(req, 'gst/category_delete.html', {'categorys': categorys})
+
+    def post(self, req):
+        n = req.POST['name']
+        Category.objects.filter(name=n).filter(user=u).delete()
+        return HTTPr('Successful deleted category')
---
+++ src/goal_set_track/gst/task_view.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.http import HttpResponse as HTTPr
+from .models import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class TaskView(LoginRequiredMixin, View):
+    def get(self, req, category):
+        viewitems = {
+            'title': 'Tareas',
+            'username': req.user.username,
+            'category': req.user.category_set.get(id=category),
+            'tasks': req.user.category_set.get(id=category).task_set.all()
+        }
+        return render(req, 'gst/task.html', viewitems)
+
+class TaskCreateView(LoginRequiredMixin, View):
+    def post(self, req, category):
+        name = req.POST['name']
+        description = req.POST.get('description', '')
+        deadline = None if req.POST.get('deadline', '') == '' else req.POST.get('deadline')
+        notify_user = True if req.POST.get('notify_user', False) else False
+        req.user.category_set.get(id=category).task_set.create(name=name, description=description,
+                                                                      deadline=deadline, notify_user=notify_user,
+                                                                      complete=False)
+        return redirect('task', category=category)
+
+class TaskEditView(LoginRequiredMixin, View):
+    def post(self, req, category, task):
+        task = req.user.category_set.get(id=category).task_set.get(id=task)
+        task.name = req.POST.get('new_name', task.name)
+        task.description = req.POST.get('description', task.description)
+        task.deadline = req.POST.get('deadline', task.deadline)
+        task.notify_user = req.POST.get('notify_user', task.notify_user)
+        task.complete = bool(req.POST.get('complete', task.complete))
+        task.save()
+        return redirect('task', category=category)
+
+class TaskDeleteView(LoginRequiredMixin, View):
+    def get(self, req):
+        viewitems = {
+        }
+        return render(req, 'task/task_delete.html', viewitems)
+
+    def post(self, req):
+        t = SubTask.objects.get(name=req.POST['name'])
+        t.delete()
+        return HTTPr('Deleted.')
