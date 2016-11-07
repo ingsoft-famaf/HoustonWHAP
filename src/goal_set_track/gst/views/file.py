8e499bb106dbdd718b547df3b8c259f5633fdd0d
nahuelbrandan <nahuelbrandan123@gmail.com>
2016-11-07 16:49:54 -0300

    little change

--- src/goal_set_track/gst/views/file.py
+++ src/goal_set_track/gst/views/file.py
@@ -1,10 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.http import HttpResponse as HTTPr
-from ..models import *
-from django.contrib.auth.models import User
-from django.contrib.auth.mixins import LoginRequiredMixin
-from deadline import number_deadlines_from_user
+
 
 class FileView(LoginRequiredMixin, View):
     def get(self, req, category, task):
@@ -17,23 +14,27 @@ class FileView(LoginRequiredMixin, View):
         }
         return render(req, 'gst/file.html', viewitems)
 
+
 class FileAddView(LoginRequiredMixin, View):
     def post(self, req, category, task):
+
+        archivo = req.FILES['file']
 
         t = req.user.category_set.get(id=category).task_set.get(id=task)
         t.file_set.create(name=req.POST['name'], file=req.POST['file'])
 
         return redirect('file', category=category, task=task)
 
+
 class FileDeleteView(LoginRequiredMixin, View):
     def post(self, req, category, task, file):
         req.user.category_set.get(id=category).task_set.get(id=task).file_set.get(id=file).delete()
-    
+
         return redirect('file', category=category, task=task)
 
 
 class FileDowloadView(LoginRequiredMixin, View):
     def post(self, req, category, task, file):
-        print "\n\n\nBBBBBBBBBB\n\n\n"
+         print "\n\n\nBBBBBBBBBB\n\n\n"
 
-#        return redirect('file', category=category, task=task)
+# return redirect('file', category=category, task=task)
